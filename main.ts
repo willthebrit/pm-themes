4fd2405c583806961a21df7fb2059c48d9be0de3
wmorgan <william.morgan@pnmac.com>
2024-11-13 11:14:08 -0800

    theme

--- main.ts
+++ main.ts
@@ -1,3 +1,60 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
+import {
+  createTheme,
+  type MantineColorsTuple,
+  type MantineTheme,
+  rem,
+} from "@mantine/core";
+
+const $marineBlue: MantineColorsTuple = [
+  "#e9f2ff",
+  "#d3e2fe",
+  "#a6c1f5",
+  "#769fed",
+  "#4f82e7",
+  "#356fe3",
+  "#2666e3",
+  "#1656ca",
+  "#0a4cb5",
+  "#0042a1",
+];
+
+export const PmTheme = createTheme({
+  autoContrast: true,
+  activeClassName: "",
+  components: {
+    InputWrapper: {
+      styles: (theme: MantineTheme) => ({
+        label: {
+          marginBottom: theme.spacing.xxs,
+          fontSize: theme.fontSizes.sm,
+        },
+      }),
+    },
+  },
+  fontFamily: "Open Sans, sans-serif",
+  headings: { fontFamily: "Open Sans, sans-serif" },
+  primaryShade: 6,
+  primaryColor: "marine-blue",
+  colors: {
+    "marine-blue": $marineBlue,
+  },
+  defaultRadius: "6px",
+  fontSizes: {
+    xs: rem(10),
+    sm: rem(12),
+    md: rem(14),
+    lg: rem(16),
+    xl: rem(18),
+  },
+  lineHeights: {
+    xs: "1.4",
+    sm: "1.45",
+    md: "1.55",
+    lg: "1.6",
+    xl: "1.65",
+  },
+  radius: { sm: "6px", md: "8px", lg: "10px" },
+  spacing: {
+    xxs: rem(4),
+  },
+});
