80d8db65b62f39a1e8e39f2d6424dac6da244ef5
wmorgan <william.morgan@pnmac.com>
2024-11-13 13:36:59 -0800

    wrong import

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ import {
   type MantineColorsTuple,
   type MantineTheme,
   rem,
-} from "npm:@mantine/core@^7.14.0";
+} from "@mantine/core";
 
 const $marineBlue: MantineColorsTuple = [
   "#e9f2ff",
